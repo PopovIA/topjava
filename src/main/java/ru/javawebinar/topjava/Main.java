8cab80020fd54158ac1903b6f7f716bcc98162e9
PopovIA <popovi-87@mail.ru>
2024-01-30 10:02:01 +0300

    ...

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,4 +1,11 @@
 package ru.javawebinar.topjava;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.util.UserMealsUtil;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @see <a href="http://topjava.herokuapp.com">Demo application</a>
@@ -7,5 +14,17 @@ package ru.javawebinar.topjava;
 public class Main {
     public static void main(String[] args) {
         System.out.format("Hello TopJava Enterprise!");
+//        UserMealsUtil.filteredByCycles()
+//        List<UserMeal> meals, LocalTime startTime, java.time.LocalTime endTime, int caloriesPerDay
+//        UserMealsUtil obj = new UserMealsUtil();
+//        List<UserMeal> mealsList = new ArrayList<UserMeal>();
+//        UserMeal eggs = new UserMeal(LocalDateTime.of(2023, 12, 20, 8, 0), "Eggs", 870);
+//        UserMeal borsch = new UserMeal(LocalDateTime.of(2023, 12, 20, 13, 0), "Borch", 1200);
+//        UserMeal lasanya = new UserMeal(LocalDateTime.of(2023, 12, 20, 20, 0), "Lasanya", 900);
+//        mealsList.add(eggs);
+//        mealsList.add(borsch);
+//        mealsList.add(lasanya);
+//        mealsList = List.of(eggs,borsch,lasanya);
+//        UserMealsUtil.filteredByCycles(mealsList, LocalTime.of(8,0), LocalTime.of(23, 0), 2300);
     }
 }
